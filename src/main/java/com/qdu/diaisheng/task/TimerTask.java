777ce436acfd18854baa17745c29703b3c2bbf4c
changliang <1246911940@qq.com>
2019-10-19 10:34:42 +0800

    更新实时摄像定时任务

---
+++ src/main/java/com/qdu/diaisheng/task/TimerTask.java
@@ -0,0 +1,120 @@
+package com.qdu.diaisheng.task;
+
+import com.alibaba.fastjson.JSONObject;
+import com.qdu.diaisheng.entity.Photo;
+import com.qdu.diaisheng.service.PhotoService;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@EnableScheduling
+@EnableAsync
+public class TimerTask {
+    public static int PORT=20001;
+    private Logger logger = Logger.getLogger(this.getClass());
+    @Autowired
+    private PhotoService photoService;
+
+    public static ServerSocket createSocket() throws IOException {
+        ServerSocket socketServer=new ServerSocket();
+        return socketServer;
+    }
+    public void initRequestConfig(){
+
+    }
+    public static void initSocket(ServerSocket serverSocket) throws SocketException {
+        serverSocket.setReuseAddress(true);
+        serverSocket.setReceiveBufferSize(6*1024*1024);
+        serverSocket.setPerformancePreferences(2,2,1);
+    }
+    @Async
+    @Scheduled(cron = "0 */5 * * * ?")
+    public void queryDatasFromDtu(){
+        try{
+            ServerSocket serverSocket=createSocket();//给云服务器制定端口，初始化
+            logger.info("监听端口成功...");
+            initSocket(serverSocket);//连接属性初始化设置
+            serverSocket.bind(new InetSocketAddress(Inet4Address.getLocalHost(),PORT),50);
+            logger.info("start server successful......"+Inet4Address.getLocalHost().toString());
+            Socket clientSocket=serverSocket.accept();// 如果有请求到达，则接受请求，并建立一个新的套接字
+            logger.info("accept tcp client "+clientSocket.getRemoteSocketAddress().toString());
+            logger.info("本地ip为:"+clientSocket.getLocalAddress().toString()+"远程ip地址为："+clientSocket.getRemoteSocketAddress().toString());
+            ClientHander hander=new ClientHander(clientSocket);
+            hander.start();
+        }catch (Exception e){
+            logger.error("spring定时任务出错！",e);
+        }
+    }
+    /**
+     * @Author wangxi
+     * @Description 定时执行任务，每半小时从接口获取图片的Base64编码并存入数据库中
+     * @Date 2019/7/21 23:35
+     * @Param [request, response]
+     * @return
+     **/
+    @Async
+    @Scheduled(cron = "0 */10 * * * ?")
+    public void timerAddPhoto(){
+        String url="http://39.108.213.89:10100/GetSnapshotPic";
+        Map<String,Object> map=new HashMap<>();
+        CloseableHttpClient client=null;
+        HttpGet httpGet=null;
+        HttpResponse response=null;
+        try{
+            String charset="UTF-8";
+            client= HttpClientBuilder.create().build();
+            httpGet=new HttpGet(url);
+            RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(50000).setConnectTimeout(50000).build();
+            httpGet.setConfig(requestConfig);
+            httpGet.addHeader("Content-Type","application/x-www-form-urlencoded");
+            httpGet.addHeader("charset",charset);
+            httpGet.setURI(URI.create(url + "?deviceid=c844150007b9&serverip=39.108.213.89"));
+            response=client.execute(httpGet);
+            String res= EntityUtils.toString(response.getEntity());
+            JSONObject jsonObject=JSONObject.parseObject(res);
+            String Base64=jsonObject.getString("picBase64");
+            SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date current=new Date();
+            String formatCurrent=sdf.format(current);
+            Photo p=new Photo();
+            p.setCameraId("c844150007b9");
+            p.setCreateTime(formatCurrent);
+            p.setContent(Base64);
+            if(photoService.addPhoto(p)>0){
+                logger.info("添加成功");
+            }
+        }catch (Exception e){
+            map.put("msg","java获取设备实时抓拍图片接口异常");
+            e.printStackTrace();
+        }finally {
+            httpGet.releaseConnection();
+            try{
+                if(client!=null){
+                    client.close();
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
---
+++ target/diaisheng-dataplatform/WEB-INF/resources/js/dataexport.js
@@ -0,0 +1,325 @@
+$(function (){
+    getDevices('00015203000000000001');
+    var urlData = decodeURI(location.search); //获取url中"?"符后的字串
+    var theRequest = new Object();
+    if (urlData.indexOf("?") != -1) {
+        var str = urlData.substr(1);
+        strs = str.split("&");
+        for(var i = 0; i < strs.length; i ++) {
+            theRequest[strs[i].split("=")[0]]=unescape(strs[i].split("=")[1]);
+        }
+    }
+    layui.use('form', function(){
+        var form = layui.form;
+        if(theRequest.congji&&theRequest.dataName){
+            $("#congji").html('<option value="">'+theRequest.congji+'</option>');
+            $("#data").html('<option value="'+theRequest.data+'">'+theRequest.dataName+'</option>');
+        }
+        form.render();
+        form.on('select(device)', function (data) {
+            getModels(data.value);
+        });
+        form.on('select(model)', function (data) {
+            getDataPoints(data.value);
+        });
+
+    });
+    laydate.render({
+        elem: '#start_time'
+        ,type: 'datetime'
+    });
+    laydate.render({
+        elem: '#end_time'
+        ,type: 'datetime'
+    });
+    var urlData = decodeURI(location.search); //获取url中"?"符后的字串
+    var theRequest = new Object();
+    if (urlData.indexOf("?") != -1) {
+        var str = urlData.substr(1);
+        strs = str.split("&");
+        for(var i = 0; i < strs.length; i ++) {
+            theRequest[strs[i].split("=")[0]]=unescape(strs[i].split("=")[1]);
+        }
+    }
+    //获取当前时间
+    var date = new Date();
+    var seperator1 = "-";
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+    var minute=date.getMinutes()<=9?"0"+date.getMinutes():date.getMinutes();
+    var initEndTime = year + seperator1 + month + seperator1 + strDate+" "+date.getHours()+":"+minute+":"+date.getSeconds();
+    var lastHour = (date.getHours()-1)<0?0:(date.getHours()-1);//判断0点时结果-1异常
+    var initStartTime = year + seperator1 + month + seperator1 + strDate+" "+lastHour+":"+minute+":"+date.getSeconds();
+    $("#start_time").val(initStartTime);
+    $("#end_time").val(initEndTime);
+    initEcharts();
+    myCharts.showLoading();
+    var name = [];
+    var series = [];
+    $.ajax({
+        type:"post",
+        url:"/diaisheng/valueadmin/getdatabetweendate",
+        data:{"data":theRequest.data,"stime":initStartTime,"etime":initEndTime},
+        dataType:"json",
+        success:function(data){
+                $.each(data.dataValue,function(key,value){
+                    console.log(data.dataValue);
+                    name.push(value.createTime);
+                });
+                $.each(data.dataValue,function(key,value){
+                    series.push(value.value);
+                });
+                myCharts.hideLoading();
+                myCharts.setOption({
+                    xAxis:{
+                        data:name
+                    },
+                    series:{
+                        data:series
+                    }
+                });
+        },
+        error: function (errorMsg) {
+            //请求失败时执行该函数
+            alert("图表请求数据失败!");
+            myChart.hideLoading();
+        }
+    });
+});
+
+function query(){
+    initEcharts();
+    var shebei = document.getElementById("shebei").value;
+    var congji = document.getElementById("congji").value;
+    var data = document.getElementById("data").value;
+    var stime = document.getElementById("start_time").value;
+    var etime = document.getElementById("end_time").value;
+    myCharts.showLoading();
+    var name = [];
+    var series = [];
+    $.ajax({
+        type:"post",
+        url:"/diaisheng/valueadmin/getdatabetweendate",
+        data:{"data":data,"stime":stime,"etime":etime},
+        dataType:"json",
+        success:function(data){
+                $.each(data.dataValue,function(key,value){
+                    name.push(value.createTime);
+                });
+                $.each(data.dataValue,function(key,value){
+                    series.push(value.value);
+                });
+                myCharts.hideLoading();
+                myCharts.setOption({
+                    xAxis:{
+                        data:name
+                    },
+                    series:{
+                        data:series
+                    }
+                });
+        },
+        error: function (data) {
+            //请求失败时执行该函数
+            alert("图表请求数据失败!");
+            myCharts.hideLoading();
+        }
+    });
+
+}
+
+function getDataPoints(modelId){//获取对应数据点列表
+    $("#data").html('<option value="">直接选择或搜索选择</option>');
+    $.ajax({
+        type: "post",
+        url: "/diaisheng/datapointadmin/getdatapoint",
+        data: {"dataModelId": modelId},
+        dataType: "json",
+        success: function (data) {
+            $.each(data.dataPointList, function (key, value) {
+                var option = new Option(value.dataPointName,value.dataPointId);
+                $('#data').append(option);//往下拉菜单里添加元素
+                renderForm();
+            });
+        },
+        error: function (data) {
+            alert("数据请求失败");
+        }
+    });
+}
+function getModels(deviceId) {//获取对应从机列表
+    $("#congji").html('<option value="">直接选择或搜索选择</option>');
+    $.ajax({
+        type: "POST",
+        url: "/diaisheng/modeladmin/getdatamodel",
+        data: {"deviceId": deviceId},
+        dataType: "json",
+        success: function (data) {
+            $.each(data.dataModelList, function (key, value) {
+                var option = new Option(value.dataModelName,value.dataModelId);
+                $('#congji').append(option);//往下拉菜单里添加元素
+                renderForm();
+            });
+        },
+        error: function (data) {
+            alert("数据请求失败");
+        }
+    });
+}
+function getDevices(deviceId){//获取设备列表
+    $("#shebei").html('<option value="">直接选择或搜索选择</option>');
+    $.ajax({
+        url: '/diaisheng/deviceadmin/getdevice',
+        type: 'POST',
+        dataType: 'json',
+        success: function (data) {
+            if (!data.success){
+                if (data.redirect){
+                    parent.window.location.href=data.redirect;
+                }
+            }else{
+                $.each(data.device, function (key, value) {
+                    var option = new Option(value.deviceName,value.deviceId);
+                    if(value.deviceId == deviceId) {
+                        option.setAttribute("selected",'true');
+                    }
+                    $('#shebei').append(option);//往下拉菜单里添加元素
+                    renderForm();
+                });
+            }
+        },
+        error:function (data) {
+            console.log(data.errMsg);
+        }
+    });
+}
+
+
+function downLoad(){
+    var shebei = document.getElementById("shebei").value;
+    var congji = document.getElementById("congji").value;
+    const selectedValueArr = document.querySelector("#data").options;
+    var stime = document.getElementById("start_time").value;
+    var etime = document.getElementById("end_time").value;
+    var data = document.getElementById("data").value;
+    console.log(shebei);
+    console.log(congji);
+    console.log(data);
+    console.log(stime);
+    console.log(etime);
+    myCharts.showLoading();
+    /*
+    $.ajax({
+        type:"post",
+        url:"/diaisheng/valueadmin/downLoadExcel",
+        data:{"data":data,"stime":stime,"etime":etime},
+        dataType:"json",
+        success:function(result){
+            myCharts.hideLoading();
+        },
+        error: function (errorMsg) {
+            //请求失败时执行该函数
+            alert("请求数据失败!");
+            alert(errorMsg);
+            myChart.hideLoading();
+        }=
+    })*/
+    var str="/diaisheng/valueadmin/downLoadExcel?"+"data="+data+"&stime="+stime+"&etime="+etime;
+    window.open(str);
+    myCharts.hideLoading();
+}
+function initEcharts() {
+    var option = {
+        title:{
+            text:'Line Test',
+            subtext:"Mountain Technology"
+        },
+        tooltip:{
+            trigger:'axis'
+        },
+        legend:{
+            data:'出水量'
+//                data:[]
+        },
+        grid:{
+            left:'3%',
+            right:'4%',
+            bottom:'3%',
+            containLabel:true
+        },
+        xAxis:{
+            type:'category',
+            //data:['18:00','18:20','18:40','18:50','19:00','19:05','19:10','19:20','19:30','19:40','19:50'],
+            data:[],
+            axisLine:{
+                lineStyle:{
+//                        color:''
+                }
+            },
+        },
+        yAxis: {
+            type: 'value'
+        },
+        dataZoom: [
+            {
+                type: 'slider',
+                xAxisIndex: 0,
+                start: 10,
+                end: 60
+            },
+            {
+                type: 'inside',
+                xAxisIndex: 0,
+                start: 10,
+                end: 60
+            },
+            {
+                type: 'slider',
+                yAxisIndex: 0,
+                start: 30,
+                end: 80
+            },
+            {
+                type: 'inside',
+                yAxisIndex: 0,
+                start: 30,
+                end: 80
+            }
+        ],
+
+        series:{
+            name:'出水量',
+            type:'line',
+            symbolSize:3,
+            color:['red'],
+            //data:[1000,200,500,600,800,1100,500,800,600,500,600],
+            data:[],
+            //关键点：实线虚线
+            smooth:true,
+            itemStyle:{
+                normal:{
+                    lineStyle:{
+                        witdth:2,
+                        type:'solid'
+                    }
+                }
+            }
+
+        }
+
+    }
+    myCharts.setOption(option);
+}
+function renderForm(){
+    layui.use('form', function(){
+        var form = layui.form;
+        form.render();
+    });
+}
